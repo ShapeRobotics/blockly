c172b8ea1f0b424afdd22f5c086f9733429d5b3d
Laverden <nlav05@gmail.com>
2022-09-26 11:47:43 +0200

    Ports Joint Senses blocks to JavaScript

---
+++ generators/javascript/senses_joint.js
@@ -0,0 +1,18 @@
+/**
+ * @fileoverview Generating JavaScript for actions/common blocks.
+ * @author nicolas@shaperobotics.com (Nicolas Laverde)
+ */
+ "use strict";
+
+goog.provide("Blockly.JavaScript.sensesJoint");
+
+goog.require("Blockly.JavaScript");
+
+Blockly.JavaScript.fable_read_joint_sensor = function (block) {
+  const order = Blockly.JavaScript.ORDER_ATOMIC;
+  const moduleID = block.getDynamicIDFieldString();
+  const metric = block.getFieldValue("METRIC");
+  const code = `api.readJointSensor(${metric}, ${moduleID})`;
+
+  return [code, order];
+};
