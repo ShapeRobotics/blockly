6105bd473a0d76bf2fc87878e9b88d39ed917e52
Laverden <nlav05@gmail.com>
2022-10-26 12:15:46 +0200

    Adds one Camera block generation, fixes setPos

--- generators/javascript/actions_joint.js
+++ generators/javascript/actions_joint.js
@@ -11,9 +11,9 @@ Blockly.JavaScript["fable_set_module_motor_positions"] = function (block) {
 	const moduleID = block.getDynamicIDFieldString();
 	const targetX = Blockly.JavaScript.valueToCode(block, "MOTOR_POSITION_X", Blockly.JavaScript.ORDER_NONE) || "None";
 	const targetY = Blockly.JavaScript.valueToCode(block, "MOTOR_POSITION_Y", Blockly.JavaScript.ORDER_NONE) || "None";
-	const code = `api.SetPos(${targetX}, ${targetY}, ${moduleID});\n`;
+	const code = `api.setPos(${targetX}, ${targetY}, ${moduleID});\n`;
 	const _codeAppendage = `api.setSpeed(50, 50, ${moduleID});\n`;
-	return "".concat(code, _codeAppendage);
+	return "".concat(_codeAppendage, code);
 }
 
 Blockly.JavaScript["fable_set_module_motor_pos_speed"] = function (block) {
---
+++ generators/javascript/camera_cv.js
@@ -0,0 +1,13 @@
+/**
+ * @fileoverview Generating JavaScript for actions/common blocks.
+ * @author nicolas@shaperobotics.com (Nicolas Laverde)
+ */
+ "use strict";
+
+goog.provide("Blockly.JavaScript.cameraCompVision");
+goog.require("Blockly.JavaScript");
+
+Blockly.JavaScript["camera_take_picture"] = function (block) {
+  const code = "api.takePicture()\n";
+  return code;
+};
