1d2d64a1ca7622a9b0d5d07e88546b988645b110
Laverden <nlav05@gmail.com>
2022-10-31 14:34:33 +0100

    Adds semicolons to Spin code generators

--- generators/javascript/actions_spin.js
+++ generators/javascript/actions_spin.js
@@ -45,7 +45,7 @@ Blockly.JavaScript["fable_set_speed_simple"] = function (block) {
         }
     }
 
-    const code = `api.setSpinSpeed(${speedA}, ${speedB}, ${moduleID})\n`;
+    const code = `api.setSpinSpeed(${speedA}, ${speedB}, ${moduleID});\n`;
     return code;
 }
 
@@ -53,7 +53,7 @@ Blockly.JavaScript["fable_spin_set_speed"] = function (block) {
     const moduleID = block.getDynamicIDFieldString();
     const speedA = Blockly.JavaScript.valueToCode(block, "MOTOR_A_SPEED", Blockly.JavaScript.ORDER_NONE) || "None";
     const speedB = Blockly.JavaScript.valueToCode(block, "MOTOR_B_SPEED", Blockly.JavaScript.ORDER_NONE) || "None";
-    const code = `api.setSpinSpeed(${speedA}, ${speedB}, ${moduleID})\n`;
+    const code = `api.setSpinSpeed(${speedA}, ${speedB}, ${moduleID});\n`;
     return code;
 }
 
@@ -61,7 +61,7 @@ Blockly.JavaScript["fable_spin_drive"] = function (block) {
     const moduleID = block.getDynamicIDFieldString();
     const distance = Blockly.JavaScript.valueToCode(block, "DISTANCE", Blockly.JavaScript.ORDER_NONE) || "None";
     const metric = block.getFieldValue("METRIC");
-    const code = `api.driveByMetric(${distance}, ${metric}, ${moduleID})\n`;
+    const code = `api.driveByMetric(${distance}, ${metric}, ${moduleID});\n`;
     return code;
 }
 
@@ -70,7 +70,7 @@ Blockly.JavaScript["fable_spin_drive_with_speed"] = function (block) {
     const distance = Blockly.JavaScript.valueToCode(block, "DISTANCE", Blockly.JavaScript.ORDER_NONE) || "None";
     const metric = block.getFieldValue("METRIC");
     const speed = Blockly.JavaScript.valueToCode(block, "SPEED", Blockly.JavaScript.ORDER_NONE) || "None";
-    const code = `api.driveByMetric(${distance}, ${metric}, ${moduleID}, ${speed})\n`;
+    const code = `api.driveByMetric(${distance}, ${metric}, ${moduleID}, ${speed});\n`;
     return code;
 }
 
@@ -78,7 +78,7 @@ Blockly.JavaScript["fable_spin_spin"] = function (block) {
     const moduleID = block.getDynamicIDFieldString();
     const turns = Blockly.JavaScript.valueToCode(block, "TURNS", Blockly.JavaScript.ORDER_NONE) || "None";
     const metric = block.getFieldValue("METRIC");
-    const code = `api.spinByMetric(${turns}, ${metric}, ${moduleID})\n`;
+    const code = `api.spinByMetric(${turns}, ${metric}, ${moduleID});\n`;
     return code;
 }
 
@@ -87,28 +87,28 @@ Blockly.JavaScript["fable_spin_spin_with_speed"] = function (block) {
     const turns = Blockly.JavaScript.valueToCode(block, "TURNS", Blockly.JavaScript.ORDER_NONE) || "None";
     const metric = block.getFieldValue("METRIC");
     const speed = Blockly.JavaScript.valueToCode(block, "SPEED", Blockly.JavaScript.ORDER_NONE) || "None";
-    const code = `api.spinByMetric(${turns}, ${metric}, ${moduleID}, ${speed})\n`;
+    const code = `api.spinByMetric(${turns}, ${metric}, ${moduleID}, ${speed});\n`;
     return code;
 }
 
 Blockly.JavaScript["fable_spin_headlights"] = function (block) {
     const moduleID = block.getDynamicIDFieldString();
     const selection = block.getFieldValue("HEADLIGHTS_ACTION");
-    const code = `api.setSpinHeadlight("${selection}", ${moduleID})\n`;
+    const code = `api.setSpinHeadlight("${selection}", ${moduleID});\n`;
     return code;
 }
 
 Blockly.JavaScript["fable_spin_set_ir_message"] = function (block) {
     const moduleID = block.getDynamicIDFieldString();
     const message = block.getField("MESSAGE").convertKeyToCode();
-    const code = `api.setSpinIrMsg(${message}, ${moduleID})\n`;
+    const code = `api.setSpinIrMsg(${message}, ${moduleID});\n`;
     return code;
 }
 
 Blockly.JavaScript["fable_spin_reset_encoder"] = function (block) {
     const moduleID = block.getDynamicIDFieldString();
     const motorID = block.getFieldValue("MOTOR_ID");
-    const code = `api.spinResetEncoder("${motorID}", ${moduleID})\n`;
+    const code = `api.spinResetEncoder("${motorID}", ${moduleID});\n`;
     return code;
 }
 
@@ -118,7 +118,7 @@ Blockly.JavaScript["fable_spin_set_constant"] = function (block) {
     const _token = "methodName"
     
     var value = Blockly.JavaScript.valueToCode(block, "VALUE", Blockly.JavaScript.ORDER_NONE);
-    var code = `api.${_token}(${value}, ${moduleID})\n`;
+    var code = `api.${_token}(${value}, ${moduleID});\n`;
 
     if (metric === "AXLE") {
         value = value || 121.38;
@@ -142,6 +142,6 @@ Blockly.JavaScript["fable_spin_spin_wheel_by_metric"] = function (block) {
   const metric = block.getFieldValue("METRIC");
   const speed = Blockly.JavaScript.valueToCode(block, "SPEED", Blockly.JavaScript.ORDER_NONE) || "None";
   const isAbsoluteReference = (reference === "absolute") ? "True" : "False";
-  const code = `api.spinWheelByMetric("${motor}", ${turns}, ${metric}, ${moduleID}, ${speed}, abs=${isAbsoluteReference})\n`;
+  const code = `api.spinWheelByMetric("${motor}", ${turns}, ${metric}, ${moduleID}, ${speed}, abs=${isAbsoluteReference});\n`;
   return code;
 }
